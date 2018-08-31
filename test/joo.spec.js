35ec52af36c85e7cded2f2a5245e75fbae0e7335
shayanypn <shayanypn@gmail.com>
2018-08-31 21:36:07 +0430

    Add testing with chai

--- test/joo.spec.js
+++ test/joo.spec.js
@@ -1,20 +1,64 @@
-const test = require('tape');
-const detect = require('../dist/joo.js');
+// const test = require('tape');
+// const detect = require('../dist/joo.js');
 
-function assertAgentString(t, agentString, expectedResult) {
-  t.deepEqual(joo.init(agentString).get(), expectedResult);
+// function assertAgentString(t, agentString, expectedResult) {
+//   t.deepEqual(joo.init(agentString).get(), expectedResult);
+// }
+
+// test('detects Chrome', function(t) {
+//   assertAgentString(t,
+//     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+//     { name: 'chrome', version: '50.0.2661', os: 'Linux' }
+//   );
+
+//   assertAgentString(t,
+//     'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+//     { name: 'chrome', version: '41.0.2228', os: 'Windows 7' }
+//   );
+
+//   t.end();
+// });
+
+/* global describe, it, before */
+
+import chai from 'chai';
+import Joo from '../dist/joo.js';
+
+chai.expect();
+const expect = chai.expect;
+
+function itUserAgent(title, agentString, expectedResult) {
+  it( title , () => {
+    expect(Joo.init(agentString).get()).to.deep.equal(expectedResult);
+  });
 }
 
-test('detects Chrome', function(t) {
-  assertAgentString(t,
-    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
-    { name: 'chrome', version: '50.0.2661', os: 'Linux' }
-  );
+describe('Given an instance of joo library', () => {
+  describe('when I need the name', () => {
+    it('should return the name', () => {
+      expect(Joo.name).to.be.equal('joo');
+    });
 
-  assertAgentString(t,
-    'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    { name: 'chrome', version: '41.0.2228', os: 'Windows 7' }
-  );
-
-  t.end();
+    it('should return the version', () => {
+      expect(Joo.version).to.be.equal('0.0.1');
+    });
+  });
 });
+
+describe('Check Mozilla', () => {
+  itUserAgent(
+    'Mozilla',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
+    { bot: false, mobile: false, name: 'chrome', version: '50.0.2661', versionNumber: 50.02661, os: 'Linux' }
+  );
+});
+
+
+
+
+
+
+
+
+
+

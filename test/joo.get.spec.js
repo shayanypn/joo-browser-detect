38c85b573cdfcac11fe3e75b4314e6151bd576a3
shayanypn <shayanypn@gmail.com>
2020-07-26 06:56:10 +0430

    fix issue of test code

--- test/joo.get.spec.js
+++ test/joo.get.spec.js
@@ -1,22 +1,20 @@
 /* global describe, it, before */
 import chai from 'chai';
-import Joo from '../dist/joo.js';
+import joo from '../dist/joo.min.js';
 
 chai.expect();
 const expect = chai.expect;
 
 function itMethodGet(title, agentString, expectedResult) {
   it( title , () => {
-    expect(Joo.init(agentString).get()).to.deep.equal(expectedResult);
+    expect(joo.init(agentString).get()).to.deep.equal(expectedResult);
   });
 }
-
 
 describe('Check get method in Chrome', () => {
   itMethodGet('Chrome/50.0.2661.102',
     'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36',
     { bot: false, mobile: false, name: 'chrome', version: '50.0.2661',versionNumber: 50.02661 , os: 'Linux' }
   );
-
 });
 
---
+++ test/joo.is.spec.js
@@ -0,0 +1,18 @@
+/* global describe, it, before */
+import chai from 'chai';
+import joo from '../dist/joo.min.js';
+
+chai.expect();
+const expect = chai.expect;
+
+describe('Check is method', () => {
+  it('is chrome > 50.0.2661.102' , () => {
+  	joo.init('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36')
+  	.is([
+	      'chrome  >  50',
+	      ], is => {
+	      	expect(is).to.be.false;
+	  });
+  });
+});
+
